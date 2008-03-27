cee2b8ea265e45fec0cfcc427c61dae311013e4c
aaronshaf@gmail.com <aaronshaf@gmail.com@2f2841a0-8b46-0410-bc53-9599d08b1f18>
2008-03-27 18:57:48 +0000

    Fixed problem with articles and glossary

--- controllers/export_controller.php
+++ controllers/export_controller.php
@@ -13,15 +13,20 @@ class ExportController extends AppController {
 		$this->set('title', __('Export',true) . ' &raquo; ' . $this->viewVars['course']['title'] . ' &raquo; ' . $this->viewVars['group']['name'] . ' &raquo; ' . Configure::read('Site.name'));
 	}
 	
+	private function prepareTextForODT($string) {
+		$string = str_replace(array('<h2','/h2'),array('<h4','/h4'),$string);
+		$string = str_replace(array('<h3','/h3'),array('<h5','/h5'),$string);
+		$string = str_replace(array('<h4','/h4'),array('<h6','/h6'),$string);
+		$string = str_replace(array('<h5','/h5'),array('<strong','/strong'),$string);
+		$string = str_replace(array('<h6','/h6'),array('<em','/em'),$string);		
+		
+		return $string;
+	}
+	
 	private function export_node_to_odt($node,$level = 1) {
 		$this->openDocument->importHTML('<h' . $level . '>' . $node['title'] . '</h' . $level . '>');
 		foreach($node['Textarea'] as $textarea) {
-			$textarea['content'] = str_replace(array('<h2','/h2'),array('<h4','/h4'),$textarea['content']);
-			$textarea['content'] = str_replace(array('<h3','/h3'),array('<h5','/h5'),$textarea['content']);
-			$textarea['content'] = str_replace(array('<h4','/h4'),array('<h6','/h6'),$textarea['content']);
-			$textarea['content'] = str_replace(array('<h5','/h5'),array('<strong','/strong'),$textarea['content']);
-			$textarea['content'] = str_replace(array('<h6','/h6'),array('<em','/em'),$textarea['content']);
-
+			$textarea['content'] = $this->prepareTextForODT($textarea['content']);
 			$this->openDocument->importHTML($textarea['content']);
 		}
 		
@@ -69,6 +74,7 @@ class ExportController extends AppController {
 		
 		foreach($articles as $article) {
 			$this->openDocument->importHTML('<h2>' . $article['Article']['title'] . '</h2>');
+			$article['Article']['content'] = $this->prepareTextForODT($article['Article']['content']);
 			$this->openDocument->importHTML($article['Article']['content']);
 		}
 	
@@ -82,6 +88,7 @@ class ExportController extends AppController {
 		$this->openDocument->importHTML('<h2>' . __('Glossary',true) . '</h2>');
 		foreach($glossary_terms as $glossary_term) {
 			$this->openDocument->importHTML('<h4>' . $glossary_term['GlossaryTerm']['term'] . '</h4>');
+			$glossary_term['GlossaryTerm']['description'] = $this->prepareTextForODT($glossary_term['GlossaryTerm']['description']);
 			$this->openDocument->importHTML($glossary_term['GlossaryTerm']['description']);
 		}
 		

75b4d0dbe8888b156b5f93ebe0d82f19c8e9b6c6
aaronshaf <aaronshaf@2f2841a0-8b46-0410-bc53-9599d08b1f18>
2008-04-12 01:37:26 +0000

    Arabic issues

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -80,14 +80,22 @@ function text_direction() {
 
 // See https://trac.cakephp.org/ticket/3603
 function stringToSlug($string) {
-	$string = low(trim(str_replace(':','',$string)));
+	//$string = low(trim(str_replace(':','',$string)));
+	//$string = @mb_convert_encoding($string, 'HTML-ENTITIES', 'utf-8');   
+
+	setlocale(LC_CTYPE, 'C');	
+	$string = low($string);
+
 	$unPretty = array('/\s?-\s?/u', '/\s?_\s?/u', '/\s?\/\s?/u', '/\s?\\\s?/u', '/\s/u', '/"/u', '/\'/u');
 	$pretty   = array('-', '-', '-', '-', '-', '', '');
-	//$unPretty = array('/\s?\\\s?/u', '/\s/u', '/"/', '/\'/');
-	//$pretty   = array('-', '-', '', '');
+	
+	//$unPretty = array('/"/u', '/\'/u');
+	//$pretty   = array('', '');
+	
 	$string = preg_replace($unPretty, $pretty, $string);
 	
 	//$string = preg_replace(array('/[^\w\s]/', '/\\s+/') , array(' ', '-'), $string);
+	
 	return trim($string); // low(...)
 }
 

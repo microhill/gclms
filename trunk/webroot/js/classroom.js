e5b7b6c139deefe076a95345f578fb21a011c478
aaronshaf <aaronshaf@2f2841a0-8b46-0410-bc53-9599d08b1f18>
2008-07-22 03:58:55 +0000

    Improving framed classroom

--- trunk/webroot/js/classroom.js
+++ trunk/webroot/js/classroom.js
@@ -325,10 +325,52 @@ GCLMS.ClassroomController = {
 				}
 			}
 		});
+	},
+	
+	toggleNodeExpansion: function() {
+		var li = this.up('li');
+
+		if(li.hasClassName('gclms-collapsed')) {
+			/*
+			$$('#' + li.getAttribute('id') + ' > ul > li').each(function(node){
+				node.displayAsBlock();
+			});
+			*/
+			li.removeClassName('gclms-collapsed');
+			li.addClassName('gclms-expanded');
+		} else if(li.hasClassName('gclms-expanded')) {
+			/*
+			$$('#' + li.getAttribute('id') + ' > ul > li').each(function(node){
+				node.hide();
+			});			
+			*/
+			li.addClassName('gclms-collapsed');
+			li.removeClassName('gclms-expanded');
+		}
+	},
+	
+	toggleNodeExpansion: function() {
+		var li = this.up('li');
+
+		if(li.hasClassName('gclms-collapsed')) {
+			/*
+			$$('#' + li.getAttribute('id') + ' > ul > li').each(function(node){
+				node.displayAsBlock();
+			});
+			*/
+			li.removeClassName('gclms-collapsed');
+			li.addClassName('gclms-expanded');
+		} else if(li.hasClassName('gclms-expanded')) {
+			/*
+			$$('#' + li.getAttribute('id') + ' > ul > li').each(function(node){
+				node.hide();
+			});			
+			*/
+			li.addClassName('gclms-collapsed');
+			li.removeClassName('gclms-expanded');
+		}
 	}
 };
-
-GCLMS.classroom = {};
 
 GCLMS.Triggers.update({
 	'div.gclms-classroom-viewport': GCLMS.ClassroomController.loadViewport,
@@ -370,7 +412,8 @@ GCLMS.Triggers.update({
 			
 			return false;
 		}
-	}
+	},
+	'#gclms-nodes-tree li img.gclms-expand-button:click': GCLMS.ClassroomController.toggleNodeExpansion
 });
 
 /*

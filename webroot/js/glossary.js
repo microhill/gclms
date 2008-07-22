e2e66c43858396798c91600d83787ba57a4dd786
aaronshaf <aaronshaf@2f2841a0-8b46-0410-bc53-9599d08b1f18>
2008-07-22 21:49:29 +0000

    Improving framed classroom

--- webroot/js/glossary.js
+++ webroot/js/glossary.js
@@ -1,9 +1,15 @@
 GCLMS.GlossaryController = {
 	addGlossaryTerm: function() {
-		self.location = '/' + document.body.getAttribute('gclms:group') + '/' + document.body.getAttribute('gclms:course') + '/glossary/add';
+		self.location = 'glossary/add';
+	},
+	
+	gotoFramedGlossaryTerm: function(event) {
+		event.stop();
+		location.href = this.getAttribute('href') + '?framed';
 	}
 }
 
 GCLMS.Triggers.update({
-	'#addGlossaryTerm:click': GCLMS.GlossaryController.addGlossaryTerm
+	'#addGlossaryTerm:click': GCLMS.GlossaryController.addGlossaryTerm,
+	'.gclms-framed ul.glossary a:click': GCLMS.GlossaryController.gotoFramedGlossaryTerm
 });
